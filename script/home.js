4341952bde708087ac3296b4ba02af038540546a
Villager A <100194237+habataku@users.noreply.github.com>
2022-03-28 09:53:55 +0900

    Update home.js

--- script/home.js
+++ script/home.js
@@ -9,7 +9,7 @@ function access(app) {
     app.search.submit.style.display = 'inline';
     app.search.input.style.removeProperty('display');
     app.search.input.placeholder = 'Search the web';
-    app.search.input.setAttribute = ('list','www');
+    app.search.input.setAttribute('list','www');
     app.header.target.setAttribute('data-page', '');
     app.nav.target.style.removeProperty('display');
     document.querySelector('#open-nav').setAttribute('data-open', '');

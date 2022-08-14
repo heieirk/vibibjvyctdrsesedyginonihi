3d427fb7b282eb2a164ad8d76dd315978b22ad07
Villager A <100194237+habataku@users.noreply.github.com>
2022-08-14 21:28:05 +0900

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://violetmk2.tk/bare/',
+    bare: 'https://holyubofficial.net/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
